117c27ab0821f493f9424c3e4f6121baa7a548cd
sicardalexis-cloud <sicard_alexis@hotmail.com>
2026-04-26 21:14:48 +0200

    taille losange

--- src/components/EdgePool.tsx
+++ src/components/EdgePool.tsx
@@ -41,8 +41,8 @@ export default function EdgePool({ char, update }: Props) {
             key={i}
             onClick={() => handleDiamondClick(i)}
             style={{
-              width: "46px",
-              height: "46px",
+              width: "44px",
+              height: "44px",
               background: i < current ? "#eab308" : "#1e2937",
               transform: "rotate(45deg)",
               borderRadius: "6px",
